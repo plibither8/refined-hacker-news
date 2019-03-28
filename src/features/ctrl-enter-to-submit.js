ed73a462dbeecfe1dffbeef210e7386bd946b813
Mihir Chaturvedi <plibither8@gmail.com>
2019-03-28 23:37:34 +0530

    Fix: ctrl-enter-to-submit on inputs as well, fix bugs

--- src/features/ctrl-enter-to-submit.js
+++ src/features/ctrl-enter-to-submit.js
@@ -1,42 +1,44 @@
 function handleKeydown(event) {
 	if ((event.ctrlKey || event.metaKey) && event.keyCode === 13) {
-		const {target} = event;
-		const form = target.parentElement;
-		form.submit();
+		event.target.form.submit();
 	}
 }
 
 function init() {
-	const fields = document.querySelectorAll('textarea');
+	const textareas = document.querySelectorAll('textarea');
+	const inputs = document.querySelectorAll('input');
+	const fields = [...textareas, ...inputs];
 
 	for (const field of fields) {
 		field.addEventListener('keydown', handleKeydown);
 	}
 
-	const observer = new MutationObserver(mutationsList => {
-		for (const mutation of mutationsList) {
-			const {addedNodes} = mutation;
-			for (const node of addedNodes) {
-				if (node.nodeType !== Node.ELEMENT_NODE) {
-					continue;
-				}
+	if (window.location.pathname === '/item') {
+		const observer = new MutationObserver(mutationsList => {
+			for (const mutation of mutationsList) {
+				const {addedNodes} = mutation;
+				for (const node of addedNodes) {
+					if (node.nodeType !== Node.ELEMENT_NODE) {
+						continue;
+					}
 
-				const textarea = node.querySelector('textarea');
-				if (textarea) {
-					textarea.addEventListener('keydown', handleKeydown);
+					const textarea = node.querySelector('textarea');
+					if (textarea) {
+						textarea.addEventListener('keydown', handleKeydown);
+					}
 				}
 			}
-		}
-	});
+		});
 
-	const commentTree = document.querySelector('table.comment-tree');
-	const observerConfig = {
-		attributes: false,
-		childList: true,
-		subtree: true
-	};
+		const commentTree = document.querySelector('table.comment-tree');
+		const observerConfig = {
+			attributes: false,
+			childList: true,
+			subtree: true
+		};
 
-	window.addEventListener('load', observer.observe(commentTree, observerConfig));
+		window.addEventListener('load', observer.observe(commentTree, observerConfig));
+	}
 
 	return true;
 }
