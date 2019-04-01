44714bea466a8db08385d99047c5e88005c5e736
Mihir Chaturvedi <plibither8@gmail.com>
2019-04-01 12:18:05 +0530

    Fix: Change classname of dead comments

--- src/features/change-dead-comments-color.js
+++ src/features/change-dead-comments-color.js
@@ -6,7 +6,7 @@ function init() {
 	for (const comment of comments) {
 		const commentHeadSpan = comment.querySelector('span.comhead');
 		if (commentHeadSpan.innerText.includes('[dead]')) {
-			comment.querySelector('div.comment span.commtext.cdd').classList.add('__rhn__dead-link-color');
+			comment.querySelector('div.comment span.commtext.cdd').classList.add('__rhn__dead-comment');
 		}
 	}
 
