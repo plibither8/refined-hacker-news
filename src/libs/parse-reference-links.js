f6c9cd9ad4d602227503bfba41077ff5612a0b9a
Mihir Chaturvedi <plibither8@gmail.com>
2019-04-11 14:03:07 +0530

    Fix: Move parser function to separate file | Refine more

---
+++ src/libs/parse-reference-links.js
@@ -0,0 +1,49 @@
+export function parseReferenceLinks(activeItem) {
+	// The array that will collect all reference links found
+	// in the form of an object, containing the link's URL and
+	// the index number.
+	const links = [];
+
+	const commentSpan = activeItem.querySelector('span.commtext');
+	const children = [...commentSpan.children]; // only collect Elements, not all nodes
+
+	// This means that there are no children in the <span> element
+	// and hence it is useless to check any further for reference links
+	// assuming that all reference links are mentioned in a separate
+	// paragraph of their own, hence creating <p> children.
+	if (children.length === 0) {
+		return links;
+	}
+
+	// This expression had been made by observing the general
+	// pattern and syntax of numbered reference links seen on HN.
+	// If this can be refined further, it would be great :)
+	const indexMarkerRegex = /^\[?(?<index>\d)[\]:.]?[:.]?$/;
+
+	for (const child of children) {
+		// Here we are making sure that the child contains an anchor element to
+		// detect whether there is in fact a link or not.
+		const link = child.querySelector('a');
+		if (!link) {
+			continue;
+		}
+
+		// The first word of the first text node of the <p> tag aftering trimming
+		// trailing whitespace.
+		const textFirstWord = child.childNodes[0].textContent.trim().split(' ')[0];
+		const matches = textFirstWord.match(indexMarkerRegex);
+
+		if (!matches) {
+			continue;
+		}
+
+		const index = Number(matches.groups.index);
+
+		links.push({
+			index,
+			href: link.href
+		});
+	}
+
+	return links;
+}
