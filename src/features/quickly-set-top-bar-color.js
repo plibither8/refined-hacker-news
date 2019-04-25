cf7e54e5639c39b72c966fa63292f3cc9ac5921f
Mihir Chaturvedi <plibither8@gmail.com>
2019-04-25 12:07:36 +0530

    Fix: Add "preview" and "set" buttons to topcolors page

--- src/features/quickly-set-top-bar-color.js
+++ src/features/quickly-set-top-bar-color.js
@@ -1,8 +1,36 @@
 import {getPageDom} from '../libs/utils';
+import {elementInScrollView} from '../libs/dom-utils';
+
+function loopAndCreate(colorRows, form) {
+	for (const row of colorRows) {
+		if (!elementInScrollView(row) && row.getBoundingClientRect().top >= 0) {
+			break;
+		}
+
+		const color = row.innerText.trim();
+
+		row.innerHTML += '<td><a href="javascript:void(0)">preview</td><td><a href="javascript:void(0)">set</td>';
+		const [preview, set] = row.querySelectorAll('a');
+
+		preview.addEventListener('click', () => {
+			const topbar = document.querySelector('table#hnmain > tbody > tr > td');
+			topbar.bgColor = color;
+		});
+
+		set.addEventListener('click', () => {
+			const topbar = document.querySelector('table#hnmain > tbody > tr > td');
+			const topColorInput = form.querySelector('input[name="topc"]');
+
+			topbar.bgColor = color;
+			topColorInput.value = color;
+			form.submit();
+		});
+
+		row.classList.add('__rhn__is-set');
+	}
+}
 
 async function init(metadata) {
-	const topbar = document.querySelector('table#hnmain > tbody > tr > td');
-
 	const userPage = await getPageDom(`https://news.ycombinator.com/user?id=${metadata.user}`);
 	const form = userPage.querySelector('form.profileform');
 	form.classList.add('__rhn__no-display');
@@ -15,17 +43,15 @@ async function init(metadata) {
 	}
 
 	const colorRowTable = document.querySelectorAll('table#hnmain > tbody > tr')[2].querySelector('table');
-	const colorRows = colorRowTable.querySelectorAll('tbody > tr');
+	let colorRows = colorRowTable.querySelectorAll('tbody > tr');
 	colorRowTable.classList.add('__rhn__top-colors');
 
-	for (const row of colorRows) {
-		const color = row.innerText.trim();
-		row.addEventListener('click', () => {
-			topColorInput.value = color;
-			topbar.bgColor = color;
-			form.submit();
-		});
-	}
+	loopAndCreate(colorRows, form);
+
+	window.addEventListener('scroll', () => {
+		colorRows = colorRowTable.querySelectorAll('tbody > tr:not(.__rhn__is-set)');
+		loopAndCreate(colorRows, form);
+	});
 
 	return true;
 }
