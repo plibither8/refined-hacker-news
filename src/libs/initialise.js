d39cde57970fddc4a4723715c9ca053d936539c7
Mihir Chaturvedi <plibither8@gmail.com>
2019-05-28 13:19:56 +0530

    Style: Change camelCase to snake_case for feature name variables

--- src/libs/initialise.js
+++ src/libs/initialise.js
@@ -1,34 +1,35 @@
+/* eslint-disable camelcase */
+
 import OptionsSync from 'webext-options-sync';
 
 // All features, imported in alphabetical order
-import autoRefresh from '../features/auto-refresh';
-import changeDeadCommentsColor from '../features/change-dead-comments-color';
-import clickCommentIndentToToggle from '../features/click-comment-indent-to-toggle';
-import clickRankToVoteUnvote from '../features/click-rank-to-vote-unvote';
-import commentsUiTweaks from '../features/comments-ui-tweaks';
-import favoriteWithoutLeavingPage from '../features/favorite-without-leaving-page';
-import fetchSubmissionTitleFromUrl from '../features/fetch-submission-title-from-url';
-import hideReadStories from '../features/hide-read-stories';
-import inputFieldTweaks from '../features/input-field-tweaks';
-import keyBindingsOnInputFields from '../features/key-bindings-on-input-fields';
-import keyBindingsOnItems from '../features/key-bindings-on-items';
-import linkifyUserAbout from '../features/linkify-user-about';
-import listHnPollsSeparately from '../features/list-hn-polls-separately';
-import loadMoreLinksInNavbar from '../features/load-more-links-in-navbar';
-import moreAccessibleFavorite from '../features/more-accessible-favorite';
-import onLinkFocusComment from '../features/on-link-focus-comment';
-import openStoryLinksInNewTab from '../features/open-story-links-in-new-tab';
-import pastChooseDate from '../features/past-choose-date';
-import prefillSubmitTitle from '../features/prefill-submit-title';
-import previewAndSetTopBarColor from '../features/preview-and-set-top-bar-color';
-import profileLinksDropdown from '../features/profile-links-dropdown';
-import replyWithoutLeavingPage from '../features/reply-without-leaving-page';
-import showItemInfoOnHover from '../features/show-item-info-on-hover';
-import showSimilarSubmissions from '../features/show-similar-submissions';
-import showTopLeadersKarma from '../features/show-top-leaders-karma';
-import showUserInfoOnHover from '../features/show-user-info-on-hover';
-import sortStories from '../features/sort-stories';
-import toggleAllCommentsAndReplies from '../features/toggle-all-comments-and-replies';
+import auto_refresh from '../features/auto-refresh';
+import change_dead_comments_color from '../features/change-dead-comments-color';
+import click_comment_indent_to_toggle from '../features/click-comment-indent-to-toggle';
+import click_rank_to_vote_unvote from '../features/click-rank-to-vote-unvote';
+import comments_ui_tweaks from '../features/comments-ui-tweaks';
+import fetch_submission_title_from_url from '../features/fetch-submission-title-from-url';
+import hide_read_stories from '../features/hide-read-stories';
+import input_field_tweaks from '../features/input-field-tweaks';
+import key_bindings_on_input_fields from '../features/key-bindings-on-input-fields';
+import key_bindings_on_items from '../features/key-bindings-on-items';
+import linkify_user_about from '../features/linkify-user-about';
+import list_hn_polls_separately from '../features/list-hn-polls-separately';
+import load_more_links_in_navbar from '../features/load-more-links-in-navbar';
+import more_accessible_favorite from '../features/more-accessible-favorite';
+import on_link_focus_comment from '../features/on-link-focus-comment';
+import open_story_links_in_new_tab from '../features/open-story-links-in-new-tab';
+import past_choose_date from '../features/past-choose-date';
+import prefill_submit_title from '../features/prefill-submit-title';
+import preview_and_set_top_bar_color from '../features/preview-and-set-top-bar-color';
+import profile_links_dropdown from '../features/profile-links-dropdown';
+import reply_without_leaving_page from '../features/reply-without-leaving-page';
+import show_item_info_on_hover from '../features/show-item-info-on-hover';
+import show_similar_submissions from '../features/show-similar-submissions';
+import show_top_leaders_karma from '../features/show-top-leaders-karma';
+import show_user_info_on_hover from '../features/show-user-info-on-hover';
+import sort_stories from '../features/sort-stories';
+import toggle_all_comments_and_replies from '../features/toggle-all-comments-and-replies';
 
 import features from './features';
 import {
@@ -39,36 +40,35 @@ import {
 } from './utils';
 
 const featureList = [
-	listHnPollsSeparately,
-	keyBindingsOnItems,
-	commentsUiTweaks,
-	favoriteWithoutLeavingPage,
-	moreAccessibleFavorite,
-	loadMoreLinksInNavbar,
-	clickCommentIndentToToggle,
-	toggleAllCommentsAndReplies,
-	changeDeadCommentsColor,
-	clickRankToVoteUnvote,
-	fetchSubmissionTitleFromUrl,
-	keyBindingsOnInputFields,
-	inputFieldTweaks,
-	linkifyUserAbout,
-	onLinkFocusComment,
-	openStoryLinksInNewTab,
-	pastChooseDate,
-	prefillSubmitTitle,
-	profileLinksDropdown,
-	showItemInfoOnHover,
-	showTopLeadersKarma,
-	showUserInfoOnHover,
-	replyWithoutLeavingPage,
-	previewAndSetTopBarColor,
-	showSimilarSubmissions,
+	list_hn_polls_separately,
+	key_bindings_on_items,
+	comments_ui_tweaks,
+	more_accessible_favorite,
+	load_more_links_in_navbar,
+	click_comment_indent_to_toggle,
+	toggle_all_comments_and_replies,
+	change_dead_comments_color,
+	click_rank_to_vote_unvote,
+	fetch_submission_title_from_url,
+	key_bindings_on_input_fields,
+	input_field_tweaks,
+	linkify_user_about,
+	on_link_focus_comment,
+	open_story_links_in_new_tab,
+	past_choose_date,
+	prefill_submit_title,
+	profile_links_dropdown,
+	show_item_info_on_hover,
+	show_top_leaders_karma,
+	show_user_info_on_hover,
+	reply_without_leaving_page,
+	preview_and_set_top_bar_color,
+	show_similar_submissions,
 
 	// Options bar (order matters)
-	sortStories,
-	hideReadStories,
-	autoRefresh
+	sort_stories,
+	hide_read_stories,
+	auto_refresh
 ];
 
 // eslint-disable-next-line no-async-promise-executor
