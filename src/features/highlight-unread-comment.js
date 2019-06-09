773afabcb63378086550f9dc6a4bf1008af4d4e4
Mihir Chaturvedi <plibither8@gmail.com>
2019-06-09 14:47:24 +0530

    Fix: Change storageArea from `local` to `sync`

--- src/features/highlight-unread-comment.js
+++ src/features/highlight-unread-comment.js
@@ -2,7 +2,7 @@ import {getAllComments} from '../libs/dom-utils';
 import {paths} from '../libs/paths';
 
 async function init(metadata) {
-	const readCommentsList = (await browser.storage.local.get('readComments')).readComments || {};
+	const readCommentsList = (await browser.storage.sync.get('readComments')).readComments || {};
 
 	const currentMilliseconds = new Date().getTime();
 	for (const [id, itemObj] of Object.entries(readCommentsList)) {
@@ -43,7 +43,7 @@ async function init(metadata) {
 		comments: [...new Set([...currentComments, ...readComments])] // remove duplication
 	}
 
-	browser.storage.local.set({readComments: readCommentsList});
+	browser.storage.sync.set({readComments: readCommentsList});
 
 	return true;
 }
