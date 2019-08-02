7e1c8597e627e7c2c93102d18b5add1f78d0a07b
Mihir Chaturvedi <plibither8@gmail.com>
2019-08-02 22:51:42 +0530

    Feat: Add 'comments/score ratio' and reverse option for sort

--- src/features/sort-stories.js
+++ src/features/sort-stories.js
@@ -1,6 +1,28 @@
 import {createOptionsBar, getGroupedStories} from '../libs/dom-utils';
 import {paths} from '../libs/paths';
 
+function updateStoriesHtml(stories) {
+	const itemlistTable = document.querySelector('table.itemlist > tbody');
+	const moreRow = itemlistTable.lastElementChild;
+	const morespaceRow = moreRow.previousElementSibling;
+
+	let extraItems = [];
+	if (window.location.pathname === '/show') {
+		extraItems = [...document.querySelectorAll('table.itemlist > tbody > tr')].slice(0, 3);
+	}
+
+	itemlistTable.innerHTML = '';
+	itemlistTable.append(...extraItems);
+
+	for (const story of stories) {
+		for (const element of story.elements) {
+			itemlistTable.append(element);
+		}
+	}
+
+	itemlistTable.append(morespaceRow, moreRow);
+}
+
 function sort() {
 	const method = document.querySelector('#sort-stories-input').value;
 	const stories = getGroupedStories(document.querySelector('table.itemlist'));
@@ -16,6 +38,16 @@ function sort() {
 			break;
 		}
 
+		case 'comments/score ratio': {
+			stories.sort((a, b) => {
+				const ratioA = (a.commentsCount ? a.commentsCount : 0) / a.score;
+				const ratioB = (b.commentsCount ? b.commentsCount : 0) / b.score;
+
+				return ratioA < ratioB ? 1 : -1;
+			});
+			break;
+		}
+
 		case 'default':
 		default: {
 			stories.sort((a, b) => a.defaultRank > b.defaultRank ? 1 : -1);
@@ -23,52 +55,47 @@ function sort() {
 		}
 	}
 
-	const itemlistTable = document.querySelector('table.itemlist > tbody');
-	const moreRow = itemlistTable.lastElementChild;
-	const morespaceRow = moreRow.previousElementSibling;
-
-	let extraItems = [];
-	if (window.location.pathname === '/show') {
-		extraItems = [...document.querySelectorAll('table.itemlist > tbody > tr')].slice(0, 3);
-	}
-
-	itemlistTable.innerHTML = '';
-
-	itemlistTable.append(...extraItems);
-
-	for (const story of stories) {
-		for (const el of story.elements) {
-			itemlistTable.append(el);
-		}
-	}
-
-	itemlistTable.append(morespaceRow, moreRow);
+	updateStoriesHtml(stories);
 }
 
 function init(metadata) {
 	const optionsBar = createOptionsBar(metadata.options.optionsBarPosition);
 	const sortLabel = document.createElement('label');
 	const sortSelect = document.createElement('select');
+	const reverseButton = document.createElement('a');
 
 	sortLabel.innerHTML = 'sort stories:&nbsp;';
 	sortLabel.setAttribute('for', 'sort-stories-input');
 
 	sortSelect.id = 'sort-stories-input';
 	sortSelect.innerHTML = '';
-	['default', 'time', 'score'].forEach(o => {
-		sortSelect.innerHTML += `<option value=${o}>${o}</option>`;
+
+	reverseButton.innerHTML = 'reverse';
+	reverseButton.href = 'javascript:void(0)';
+
+	[
+		'default',
+		'time',
+		'score',
+		'comments/score ratio'
+	].forEach(opt => {
+		sortSelect.innerHTML += `<option value="${opt}">${opt}</option>`;
 	});
 
 	if (document.querySelector('#auto-refresh-input')) {
 		const {firstChild} = optionsBar;
 		optionsBar.insertBefore(sortLabel, firstChild);
 		optionsBar.insertBefore(sortSelect, firstChild);
+		optionsBar.insertBefore(reverseButton, firstChild);
 	} else {
-		optionsBar.append(sortLabel, sortSelect);
+		optionsBar.append(sortLabel, sortSelect, reverseButton);
 	}
 
-	sort();
 	sortSelect.addEventListener('change', sort);
+	reverseButton.addEventListener('click', () => {
+		const stories = getGroupedStories(document.querySelector('table.itemlist'));
+		updateStoriesHtml(stories.reverse());
+	})
 
 	return true;
 }
