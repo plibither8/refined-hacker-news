c1c2ec5ed729710899595bee959bd8f7da0bb6eb
Jerome V <jerry@vzr.me>
2019-09-18 07:43:42 +0200

    Feat: Make child's root comment collapseable
    Adds a [collapse root] button on child comments, which collapses that comment's root.

---
+++ src/features/collapse-root-comment.js
@@ -0,0 +1,58 @@
+import {getAllComments} from '../libs/dom-utils';
+import {paths} from '../libs/paths';
+
+function getCommentIndentation(element) {
+	const indentation = element.querySelector('.ind img').width / 40;
+	return indentation;
+}
+
+function elementPosition(el) {
+	const bodyRect = document.body.getBoundingClientRect();
+	const rect = el.getBoundingClientRect();
+	const top = rect.top - bodyRect.top;
+	return { x: rect.left, y: top };
+}
+
+async function init(metadata) {
+	const comments = getAllComments();
+	let currentRootComment;
+	
+	for (const comment of comments) {
+
+		const indentLevel = getCommentIndentation(comment);
+		
+		if (indentLevel === 0) {
+			currentRootComment = comment;
+			continue;
+		}
+		
+		const inst_currentRootComment = currentRootComment;
+		const toggle = document.createElement('a');
+		
+		toggle.innerText = '[collapse root]';
+		toggle.href = 'javascript:void(0)';
+		toggle.classList.add('__rhn__collapse-root-comment');
+		toggle.addEventListener('click', () => {
+			inst_currentRootComment.querySelector('a.togg').click();
+			const { x, y } = elementPosition(inst_currentRootComment);
+			window.scrollTo(x, y);
+		});
+		
+		comment.querySelector('span.comhead').append(toggle);
+		
+	}
+	
+	return true;
+}
+
+const details = {
+	id: 'collapse-root-comment',
+	pages: {
+		include: paths.comments,
+		exclude: []
+	},
+	loginRequired: false,
+	init
+};
+
+export default details;
