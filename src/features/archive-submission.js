7944cea643fcc0dd3b007ca9d19d55f7fb3bfa1d
Mihir Chaturvedi <plibither8@gmail.com>
2019-12-15 18:18:20 +0530

    Fix: Finalise working version

--- src/features/archive-submission.js
+++ src/features/archive-submission.js
@@ -1,16 +1,34 @@
-import {paths} from '../libs/paths';
-
 function init(metadata) {
+	if (metadata.path === '/item' && metadata.item.type === 'story') {
+		const storyLink = document.querySelector('a.storylink').href;
 
-	return true;
+		const separatorPipe = document.createTextNode(' | ');
+		const archiveButton = document.createElement('a');
+
+		archiveButton.innerText = 'archive';
+		archiveButton.href = 'https://wayback.now.sh/' + encodeURIComponent(storyLink);
+		archiveButton.target = '_blank';
+		archiveButton.classList.add('__rhn__archive-button');
+
+		const targetParent =
+			document.querySelector('table.fatitem td.subtext') ||
+			document.querySelector('table.fatitem span.comhead');
+		const targetSibling = targetParent.querySelector('a[href^="https://www.google.com"]').nextSibling;
+
+		targetParent.insertBefore(separatorPipe, targetSibling);
+		targetParent.insertBefore(archiveButton, targetSibling);
+
+		return true;
+	}
+
+	return false;
 }
 
 const details = {
 	id: 'archive-submission',
 	pages: {
 		include: [
-			'/item',
-			...paths.stories,
+			'/item'
 		],
 		exclude: []
 	},
--- src/libs/utils.js
+++ src/libs/utils.js
@@ -124,7 +124,7 @@ export const getOptions = new Promise(async resolve => {
 	if (options.logging) {
 		options.log = (method, ...content) => {
 			console[method](...content);
-		}
+		};
 	} else {
 		options.log = () => {};
 	}
